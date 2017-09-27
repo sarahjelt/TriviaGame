cbc8df5b9ee2fbc042ba96dbd7b87932edc06565
Sara Hjelt <sarahjelt@Saras-MacBook-Air.local>
2017-09-26 23:37:52 -0400

    reorganized object, made global functions methods

--- assets/javascript/app copy.js
+++ assets/javascript/app copy.js
@@ -76,25 +76,127 @@
 // }
 
 //if correct answer chosen run winner
-// $("input").on("click", function () {
-//   if ($(":radio.correct").is(":checked")) {
-//     console.log("yay");
-//     winner();
-//   }
-// });
+$("input").on("click", function () {
+  if ($(":radio.correct").is(":checked")) {
+    console.log("yay");
+    winner();
+  }
+});
 
-// //if incorrect answer chosen run loser
-// $("input").on("click", function () {
-//   if (($(":radio").not(".correct")).is(":checked")) {
-//     console.log("newp");
-//     loser();
-//   }
-// });
+//if incorrect answer chosen run loser
+$("input").on("click", function () {
+  if (($(":radio").not(".correct")).is(":checked")) {
+    console.log("newp");
+    loser();
+  }
+});
 
 //stop countdown -- now in new object
 // function stop() {
 //   clearInterval(intervalId);
 // }
+
+//questions and answer choices object
+// var qAndA = {
+// 	questions: {
+// 		question1: '1. Which Sailor Scout is super brainy and hopes to be a doctor someday?',
+// 		question2: "2. In the DiC Entertainment (1990s) dub of Sailor Moon, what is Nephlite's human-disguise alter ego's name?",
+// 		question3: "3. What is the name of the evil queen in the first season story arc?",
+// 		question4: "4. Usagi, Sailor Moon's normal-teenage-girl name, means what in Japanese?",
+// 		question5: "",
+// 		question6: "",
+// 		question7: "",
+// 		question8: "",
+// 		question9: "",
+// 		question10: ""
+// 	},
+// 	answers: {
+// 		answer1: {
+// 			answer1a: " A. Sailor Mars — Rei Hino",
+// 			answer1b: " B. Sailor Mercury — Ami Mizuno",
+// 			answer1c: " C. Sailor Jupiter — Makoto Kino",
+// 			answer1d: " D. Sailor Venus — Minako Aino"
+// 		},
+// 		answer2: {
+// 			answer2a: " A. Macaulay Culkin",
+// 			answer2b: " B. Maxfield Stanton",
+// 			answer2c: " C. Maxwell Caulfield",
+// 			answer2d: " D. Holden Caulfield"
+// 		},
+// 		answer3: {
+// 			answer3a: " A. Queen Barrell",
+// 			answer3b: " B. Queen Emerald",
+// 			answer3c: " C. Queen Beryl",
+// 			answer3d: " D. Queen Barra"
+// 		},
+// 		answer4: {
+// 			answer4a: " A. Bunny",
+// 			answer4b: " B. Kitten",
+// 			answer4c: " C. Moon",
+// 			answer4d: " D. Cupcake"
+// 		},
+// 		answer5: {
+// 			answer5a: " A. ",
+// 			answer5b: " B. ",
+// 			answer5c: " C. ",
+// 			answer5d: " D. "
+// 		},
+// 		answer6: {
+// 			answer6a: " A. ",
+// 			answer6b: " B. ",
+// 			answer6c: " C. ",
+// 			answer6d: " D. "
+// 		},
+// 		answer7: {
+// 			answer7a: " A. ",
+// 			answer7b: " B. ",
+// 			answer7c: " C. ",
+// 			answer7d: " D. "
+// 		},
+// 		answer8: {
+// 			answer8a: " A. ",
+// 			answer8b: " B. ",
+// 			answer8c: " C. ",
+// 			answer8d: " D. "
+// 		},
+// 		answer9: {
+// 			answer9a: " A. ",
+// 			answer9b: " B. ",
+// 			answer9c: " C. ",
+// 			answer9d: " D. "
+// 		},
+// 		answer10: {
+// 			answer10a: " A. ",
+// 			answer10b: " B. ",
+// 			answer10c: " C. ",
+// 			answer10d: " D. "
+// 		}
+// 	},
+// 	screen: {
+// 		screen1: "Sailor Mercury (Ami) wants to be a doctor like her mother. She loves to read, study, and play chess.",
+// 		screen2: "",
+// 		screen3: "",
+// 		screen4: "",
+// 		screen5: "",
+// 		screen6: "",
+// 		screen7: "",
+// 		screen8: "",
+// 		screen9: "",
+// 		screen10: ""
+// 	},
+// 	newQ: function() {
+// 	//questionable functions
+// 	for (var i = 1; i < qs.length + 1; i++) {
+// 	  grandpa = "qAndA.questions.question" + i;
+// 	  console.log(grandpa);
+// 	  $("#question").attr("qAndA.questions.question" + (i+1));
+// 	  }
+// 	}
+
+// if ($("#question").html(qAndA.questions.question1)) {
+//   $(".radio2").attr("class", "correct");
+// }
+//}//end OLD qAndA object
 
 var game = {
   questions: [
@@ -210,36 +312,33 @@ var game = {
     }
   ], //end questions array
 
-  //number: 31000,
-  numberNormal: 10000 / 1000,
-  // intervalId: 0,
+  number: 31000,
+  numberNormal: this.number / 1000,
+  intervalId: null,
   currentQuestion: 0,
-  intervalId: setInterval(this.decrement, 1000),
 
-  // reset: function() {
-  //   this.number = 30000;
-  //   $("#time").html(game.numberNormal);
-  //   game.run();
-  // },
+  reset: function() {
+    this.number = 30000;
+    $("#time").html(this.numberNormal);
+    run();
+  },
 
   //begin countdown
   run: function() {
-    intervalId = setInterval(this.decrement, 1000);
+    this.intervalId = setInterval(this.decrement, 1000);
   },
 
   //stop countdown
   stop: function() {
-    clearInterval(game.intervalId);
-    console.log(game.numberNormal);
+    clearInterval(this.intervalId);
   },
 
   decrement: function() {
-    $("#time").text(game.numberNormal);
-    // console.log(game.numberNormal);
-    game.numberNormal--;
-    if (game.numberNormal === 0) {
-      game.stop();
-      game.loser();
+    this.numberNormal--;
+    $("#time").html(this.numberNormal);
+    if (this.numberNormal === 0) {
+      stop();
+      loser();
     }
   },
 
@@ -247,20 +346,20 @@ var game = {
     $("#question").empty().append("<img src='assets/images/answer1img.png' width='200px'>");
     $(".choices").empty().append('Sorry! ' + game.questions[0].screen);
     console.log("nooooo");
-    // game.stop();
-    game.next();
+    stop();
+    next();
   },
 
   winner: function() {
     $("#question").empty().append("<img src='assets/images/answer1img.png' width='200px'>");
     $(".choices").empty().append('Great job! ' + game.questions[0].screen);
     console.log("yippee");
-    game.stop();
-    // game.next();
+    stop();
+    next();
   },
 
   next: function() {
-    this.intervalId = setTimeout(game.reset, 5000);
+    this.intervalId = setTimeout(reset, 5000);
   },
 
   changeQuestion: function() {
